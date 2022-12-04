50101201bdffb8f4dc182dc33870dc73911b79ef
usmc451982 <courtney.jackson45@outlook.com>
2022-12-04 17:33:06 -0600

    Day 3 Exercise 1

---
+++ Day 3/Conditional Operators.py
@@ -0,0 +1,16 @@
+#Lesson 1: Control Flow with if / else and Conditional Operators
+#Operator         Meaning
+#  >              Greater than
+#  <              Less than
+# >=              Greater than or equal to
+# <=              Less than or equal to
+# ==              Equal to
+# !=              Not equal to
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("You can ride the rollercoaster!")
+else:
+    print("Sorry, you're not tall enough for this ride.")
