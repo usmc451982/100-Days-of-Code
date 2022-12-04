d7dd85efa179452db7c64cc03da42e57d9d48bc7
usmc451982 <courtney.jackson45@outlook.com>
2022-12-04 13:16:36 -0600

    Day 2

---
+++ Day 2/Data Types.py
@@ -0,0 +1,14 @@
+#Data Types
+
+#String
+print("Hello"[4])
+#This is a string that will print out the 4th character starting at Zero
+
+#Integer - are numbers
+print(123 + 345)
+#Without quotes, the above code will act as a math problem and produce a solution
+
+#Float - Numbers with decimal points.  Float means the decimal point can be ANYWHERE in the number
+#Pi - 3.14159 is a float number
+
+#Boolean - Only two possible outcomes:  True or False
---
+++ Day 2/Type Error.py
@@ -0,0 +1,9 @@
+#num_char = len(input("What is your name?"))
+#print("Your name has " + num_char + " characters.")
+#The code above will produce an error
+
+a = float(123)
+print(type(a))
+
+#print(70 + float("100.5"))
+print(str(70)+ str(100))
