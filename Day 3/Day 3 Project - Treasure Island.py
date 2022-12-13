7f9184acc09924b36459085b3db0d8b405810f24
usmc451982 <courtney.jackson45@outlook.com>
2022-12-12 19:14:44 -0600

    Day 3 complete - playing catch up

--- Day 3/Day 3 Project - Treasure Island.py
+++ Day 3/Day 3 Project - Treasure Island.py
@@ -27,4 +27,20 @@ print("Your mission is to find the treasure.")
 
 #Write your code below this line 👇
 
-fork = input("You've come to a fork in the path.  Which direction do you take? left or right? \n")
+choice1 = input('You\'re at a cross road. Where do you want to go? Type "left" or "right" \n').lower()
+if choice1 == "left":
+  choice2 = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across. \n').lower()
+  if choice2 == "wait":
+    choice3 = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? \n").lower()
+    if choice3 == "red":
+      print("It's a room full of fire. Game Over.")
+    elif choice3 == "yellow":
+      print("You found the treasure! You Win!")
+    elif choice3 == "blue":
+      print("You enter a room full of beasts. Game Over.")
+    else:
+      print("You chose a door that doesn't exist. Game Over.")
+  else:
+    print("You get attacked by an angry gator. Game Over.")
+else:
+  print("You fell into a hole filled with snakes. Game Over.")
