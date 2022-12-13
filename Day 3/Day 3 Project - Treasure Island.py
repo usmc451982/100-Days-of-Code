cab5d384d3f57ea84cf21dd339943aa2c87b1952
usmc451982 <courtney.jackson45@outlook.com>
2022-12-12 19:29:23 -0600

    Final

--- Day 3/Day 3 Project - Treasure Island.py
+++ Day 3/Day 3 Project - Treasure Island.py
@@ -27,20 +27,20 @@ print("Your mission is to find the treasure.")
 
 #Write your code below this line 👇
 
-choice1 = input('You\'re at a cross road. Where do you want to go? Type "left" or "right" \n').lower()
+choice1 = input('You\'re at a cross road. Where do you want to go? Type "left" or "right" \n').lower() #The backslash "\" is used to "escape" so it doesn't think it's code
 if choice1 == "left":
-  choice2 = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across. \n').lower()
+  choice2 = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across. \n').lower() #.lower() is used to catch the input no matter how they type it out
   if choice2 == "wait":
     choice3 = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? \n").lower()
     if choice3 == "red":
-      print("It's a room full of fire. Game Over.")
+      print("It's a room with a Troll. Game Over.")
     elif choice3 == "yellow":
       print("You found the treasure! You Win!")
     elif choice3 == "blue":
-      print("You enter a room full of beasts. Game Over.")
+      print("You enter a room with Medusa. Game Over.")
     else:
       print("You chose a door that doesn't exist. Game Over.")
   else:
     print("You get attacked by an angry gator. Game Over.")
 else:
-  print("You fell into a hole filled with snakes. Game Over.")
+  print("You rolled your ankle on a bolder. Game Over.")
